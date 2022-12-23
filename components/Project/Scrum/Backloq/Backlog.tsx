048c1525643272a3c049cc31c70d21b58acaa2d6
Sameep <sameepsawant10@gmail.com>
2022-12-23 20:29:42 +0530

    edit and delete option added

--- components/Project/Scrum/Backloq/Backlog.tsx
+++ components/Project/Scrum/Backloq/Backlog.tsx
@@ -5,9 +5,8 @@ import IssueModal from "../../../Modals/IssueModal";
 import { ProjectState } from "../../../../Context/ProjectContext";
 import axios from "axios";
 import { urlFetcher } from "../../../../utils/Helper/urlFetcher";
-import Image from "next/image";
-import { colorFetcher } from "../../../../utils/Helper/colorFetcher";
-import { BiDotsVerticalRounded } from "react-icons/bi";
+
+import Issue from "../Backloq/Issue";
 
 const Backlog = () => {
   const {
@@ -63,37 +62,7 @@ const Backlog = () => {
           {issues ? (
             <div>
               {issues.map((issue, index: number) => {
-                return (
-                  <div
-                    className="w-full p-2 flex justify-between border-[2px] mt-1 border-gray-200 cursor-pointer"
-                    key={index}
-                  >
-                    <div className="flex space-x-3 items-center">
-                      <div
-                        className="w-3 h-3 rounded-sm"
-                        style={{
-                          backgroundColor: `${colorFetcher(issue.type)}`,
-                        }}
-                      ></div>
-                      <div className=" text-xs">NEW {issue.id}</div>
-                      <div className="text-sm">{issue.issue}</div>
-                    </div>
-                    <div className="flex space-x-3 items-center">
-                      <div className="py-1 px-4 bg-gray-300 rounded-sm text-xs">
-                        {issue.sectionName}
-                      </div>
-                      <div className="w-7 h-7 rounded-full items-center flex overflow-hidden">
-                        <Image
-                          src={issue.profile}
-                          width={100}
-                          height={100}
-                          alt="UserProfile"
-                        />
-                      </div>
-                      <BiDotsVerticalRounded />
-                    </div>
-                  </div>
-                );
+                return <Issue issue={issue} index={index} />;
               })}
             </div>
           ) : (
