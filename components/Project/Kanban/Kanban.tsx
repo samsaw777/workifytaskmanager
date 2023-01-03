3cf6984025973bdff9487b7feb6578f021757ba3
Sameep <sameepsawant10@gmail.com>
2023-01-03 17:58:41 +0530

    update section title completed

--- components/Project/Kanban/Kanban.tsx
+++ components/Project/Kanban/Kanban.tsx
@@ -49,9 +49,14 @@ const Kanban = () => {
 
   return (
     <div className="px-2 mx-2 w-full">
-      <div onClick={() => createSection()}>Create Section</div>
+      <div
+        onClick={() => createSection()}
+        className="p-2 bg-blue-500 text-white w-fit rounded-md cursor-pointer"
+      >
+        Create Section
+      </div>
       <DragDropContext onDragEnd={onDragEnd}>
-        <div className="flex space-x-5 w-full overflow-x-auto mt-2 h-[70vh] pb-2">
+        <div className="flex space-x-5 w-full overflow-x-auto mt-2 h-[75vh] pb-2 section-title">
           {sections.map((section: any, index: number) => {
             return (
               <div key={index}>
@@ -61,6 +66,7 @@ const Kanban = () => {
                   issues={
                     board[1].type == "KANBAN" ? section.tasks : section.issues
                   }
+                  boardId={section.boardId}
                   type="KANBAN"
                 />
               </div>
