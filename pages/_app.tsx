6abd7acbfff874d945fe124ce5711574f26c761a
Sameep <sameepsawant10@gmail.com>
2023-03-18 23:37:29 +0530

    added the loading bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,14 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Toaster } from "react-hot-toast";
 import ProjectContext from "../Context/ProjectContext";
+import NextNProgress from "nextjs-progressbar";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  // NextNProgress.configure({ showSpinner: false });
   return (
     <ProjectContext>
       <Toaster />
+      <NextNProgress color="#28BEBD" options={{ showSpinner: false }} />
       <Component {...pageProps} />
     </ProjectContext>
   );
