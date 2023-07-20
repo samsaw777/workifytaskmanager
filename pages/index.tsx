a7a6c50c1966c87c638ba9966ed773695be34182
Sameep <sameepsawant10@gmail.com>
2023-07-21 00:11:56 +0530

    fix: refactored the index.tsx code to make it load faster

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,52 +1,54 @@
-import type { NextPage } from "next";
+import { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import Nookies from "nookies";
+import { GetServerSidePropsContext, GetServerSidePropsResult } from "next";
 import * as jwt from "jsonwebtoken";
 import prisma from "../lib/prisma";
+
 const secret = process.env.JWT_SECRET || "workify";
+
 if (!secret) {
   throw new Error("No Secret");
 }
 
-const Home: NextPage = ({ loggedinUser }: any) => {
-  return <div>This is landing page.</div>;
+interface HomeProps {
+  loggedinUser?: any; // Adjust the type accordingly based on the actual user data structure
+}
+
+const Home: NextPage<HomeProps> = ({ loggedinUser }) => {
+  return <div>This is the landing page.</div>;
 };
 
 export default Home;
 
-export async function getServerSideProps(context: any) {
+export async function getServerSideProps(
+  context: GetServerSidePropsContext
+): Promise<GetServerSidePropsResult<HomeProps>> {
   const { req, res } = context;
 
   const cookie = Nookies.get({ req });
 
-  if (!cookie.token) {
-    return {
-      redirect: {
-        destination: "/login",
-        permanent: false,
-      },
-    };
-  } else {
-    return {
-      redirect: {
-        destination: "/dashboard",
-        permanent: false,
-      },
-    };
+  if (cookie.token) {
+    try {
+      // Redirect to dashboard if logged in
+      return {
+        redirect: {
+          destination: "/dashboard",
+          permanent: false,
+        },
+      };
+    } catch (error) {
+      // Handle JWT verification errors here.
+      console.error("Error verifying JWT token:", error);
+    }
   }
 
-  const jwtToken: any = jwt.verify(cookie.token, secret);
-
-  const response = await prisma.user.findUnique({
-    where: { id: jwtToken.userId },
-  });
-
-  const user: any = JSON.stringify(response);
-
+  // Redirect to login if not logged in
   return {
-    props: {
-      loggedinUser: JSON.parse(user),
+    redirect: {
+      destination: "/login",
+      permanent: false,
     },
   };
 }
